1fca497fdd8a4dd6651f6a9a02436b896d664ee1
John McSwain <john.i.mcswain@gmail.com>
2015-12-16 01:40:30 -0500

    Updating the path to include the 'asset' prefix to reduce confusion

--- js/data.js
+++ js/data.js
@@ -1,147 +1,147 @@
 var data = {
   "list": [
     {
-      "image": "the-strategy-1080527_640.jpg",
+      "image": "assets/the-strategy-1080527_640.jpg",
       "name": "the strategy"
     },
     {
-      "image": "plasterer-bee-987982_640.jpg",
+      "image": "assets/plasterer-bee-987982_640.jpg",
       "name": "plasterer bee"
     },
     {
-      "image": "nectarine-1074997_640.jpg",
+      "image": "assets/nectarine-1074997_640.jpg",
       "name": "nectarine"
     },
     {
-      "image": "moto-1084694_640.jpg",
+      "image": "assets/moto-1084694_640.jpg",
       "name": "motorcycle"
     },
     {
-      "image": "jet-887884_640.jpg",
+      "image": "assets/jet-887884_640.jpg",
       "name": "jet"
     },
     {
-      "image": "girl-1039532_640.jpg",
+      "image": "assets/girl-1039532_640.jpg",
       "name": "the girl"
     },
     {
-      "image": "dog-903990_640.jpg",
+      "image": "assets/dog-903990_640.jpg",
       "name": "the dog"
     },
     {
-      "image": "coffee-983955_640.jpg",
+      "image": "assets/coffee-983955_640.jpg",
       "name": "coffee"
     },
     {
-      "image": "chocolate-cupcake-1014635_640.jpg",
+      "image": "assets/chocolate-cupcake-1014635_640.jpg",
       "name": "chocolate cupcake"
     },
     {
-      "image": "botswana-927284_640.jpg",
+      "image": "assets/botswana-927284_640.jpg",
       "name": "botswana"
     },
     {
-      "image": "binding-contract-948442_640.jpg",
+      "image": "assets/binding-contract-948442_640.jpg",
       "name": "binding contract"
     },
     {
-      "image": "barcelona-953904_640.jpg",
+      "image": "assets/barcelona-953904_640.jpg",
       "name": "barcelona"
     },
     {
-      "image": "astronaut-877306_640.jpg",
+      "image": "assets/astronaut-877306_640.jpg",
       "name": "astronaut"
     },
     {
-      "image": "apple-1051018_640.jpg",
+      "image": "assets/apple-1051018_640.jpg",
       "name": "apple"
     },
     {
-      "image": "bass-912741_640.jpg",
+      "image": "assets/bass-912741_640.jpg",
       "name": "bass"
     },
     {
-      "image": "86H.jpg",
+      "image": "assets/86H.jpg",
       "name": "thank you"
     },
     {
-      "image": "14H.jpg",
+      "image": "assets/14H.jpg",
       "name": "vw"
     },
     {
-      "image": "17H.jpg",
+      "image": "assets/17H.jpg",
       "name": "fly away"
     },
     {
-      "image": "33H.jpg",
+      "image": "assets/33H.jpg",
       "name": "red chairs"
     },
     {
-      "image": "42H.jpg",
+      "image": "assets/42H.jpg",
       "name": "hotel sign"
     },
     {
-      "image": "48H.jpg",
+      "image": "assets/48H.jpg",
       "name": "street light"
     },
     {
-      "image": "51H.jpg",
+      "image": "assets/51H.jpg",
       "name": "bridges"
     },
     {
-      "image": "54H.jpg",
+      "image": "assets/54H.jpg",
       "name": "old truck"
     },
     {
-      "image": "69H.jpg",
+      "image": "assets/69H.jpg",
       "name": "red door"
     },
     {
-      "image": "86H.jpg",
+      "image": "assets/86H.jpg",
       "name": "thank you"
     },
     {
-      "image": "105H.jpg",
+      "image": "assets/105H.jpg",
       "name": "skates"
     },
     {
-      "image": "118H.jpg",
+      "image": "assets/118H.jpg",
       "name": "girl"
     },
     {
-      "image": "129H.jpg",
+      "image": "assets/129H.jpg",
       "name": "vintage car"
     },
     {
-      "image": "154H.jpg",
+      "image": "assets/154H.jpg",
       "name": "landrymat"
     },
     {
-      "image": "162H.jpg",
+      "image": "assets/162H.jpg",
       "name": "escalators"
     },
     {
-      "image": "171H.jpg",
+      "image": "assets/171H.jpg",
       "name": "orange balloon"
     },
     {
-      "image": "186H.jpg",
+      "image": "assets/186H.jpg",
       "name": "briefcase"
     },
     {
-      "image": "199H.jpg",
+      "image": "assets/199H.jpg",
       "name": "red umbrellas"
     },
     {
-      "image": "230H.jpg",
+      "image": "assets/230H.jpg",
       "name": "ferris wheel"
     },
     {
-      "image": "233H.jpg",
+      "image": "assets/233H.jpg",
       "name": "girl"
     },
     {
-      "image": "239H.jpg",
+      "image": "assets/239H.jpg",
       "name": "gears"
     }
   ]
